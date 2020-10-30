4c4ec4f867ed0c3c4333e340637cb45e332e0755
zhw <953589050@qq.com>
2020-10-30 21:03:50 +0800

    实现了项目管理的业务逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -114,18 +114,25 @@ export const asyncRoutes = [
     children: [
       {
         path: 'project-management',
-        component: ()=>import('@/views/project-management/index'),
+        component: () => import('@/views/project-management/index'),
         name: 'projectManagement',
-        meta: { title: '项目管理', icon: 'list', noCache: true },
+        meta: { title: '项目管理', icon: 'list', noCache: true }
       },
       {
         path: 'project-detail',
         component: () => import('@/views/project-detail-list/index'),
         name: 'projectDetail',
         hidden: true,
-        meta: { title: '项目详情', noCache: true },
-      },   
-      ]
+        meta: { title: '项目详情', noCache: true }
+      },
+      {
+        path: 'detail-data',
+        component: () => import('@/views/detail-data/index'),
+        name: 'detailData',
+        hidden: true,
+        meta: { title: '具体数据', noCache: true }
+      }
+    ]
   },
   // menu->数据分析
   {
@@ -135,7 +142,7 @@ export const asyncRoutes = [
     children: [
       {
         path: 'data-analysis',
-        component: ()=>import('@/views/documentation/index'),
+        component: () => import('@/views/documentation/index'),
         name: 'dataAnalysis',
         meta: { title: '数据分析', icon: 'component', noCache: true }
       }
--- src/views/project-detail-list/index.vue
+++ src/views/project-detail-list/index.vue
@@ -10,12 +10,12 @@
       <el-select v-model="listQuery.type" placeholder="测试任务" clearable class="filter-item" style="width: 200px">
         <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
       </el-select>
-        <!--以上二选一，听需求!-->
+      <!--以上二选一，听需求!-->
 
       <el-select v-model="listQuery.importance" placeholder="测试次数序号" clearable style="width: 150px" class="filter-item">
         <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
       </el-select>
-        <!--
+      <!--
       <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
         <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
       </el-select>
@@ -67,6 +67,14 @@
           <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
         </template>
       </el-table-column>
+      <el-table-column label="具体数据表格" width="150px" align="center">
+        <!--<template slot-scope="{row,$index}">!-->
+        <template>
+          <router-link class="pan-btn yellow-btn" to="/project/detail-data">
+            查看数据
+          </router-link>
+        </template>
+      </el-table-column>
       <!--
       <el-table-column label="Author" width="110px" align="center">
         <template slot-scope="{row}">
@@ -95,17 +103,19 @@
       !-->
       <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
-          <el-button type="primary" size="mini" @click="handleUpdate(row)">
-            Edit
+          <el-button type="primary" @click="handleUpdate(row)">
+            编辑
           </el-button>
+          <!--
           <el-button v-if="row.status!='published'" size="mini" type="success" @click="handleModifyStatus(row,'published')">
             Publish
           </el-button>
           <el-button v-if="row.status!='draft'" size="mini" @click="handleModifyStatus(row,'draft')">
             Draft
           </el-button>
-          <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
-            Delete
+          !-->
+          <el-button v-if="row.status!='deleted'" type="danger" @click="handleDelete(row,$index)">
+            删除
           </el-button>
         </template>
       </el-table-column>
@@ -114,36 +124,30 @@
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
 
     <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="Type" prop="type">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px" style="width: 400px; margin-left:50px;">
+        <el-form-item label="被试用户" prop="type">
           <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
             <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
           </el-select>
         </el-form-item>
-        <el-form-item label="Date" prop="timestamp">
-          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />
-        </el-form-item>
-        <el-form-item label="Title" prop="title">
+        <el-form-item label="测试任务" prop="title">
           <el-input v-model="temp.title" />
         </el-form-item>
-        <el-form-item label="Status">
-          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
-            <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="Imp">
+        <el-form-item label="测试次数序号">
           <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />
         </el-form-item>
+        <!--
         <el-form-item label="Remark">
           <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />
         </el-form-item>
+        !-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">
-          Cancel
+          取消
         </el-button>
         <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
-          Confirm
+          确认
         </el-button>
       </div>
     </el-dialog>
